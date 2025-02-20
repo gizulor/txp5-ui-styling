b6a2fb98c9bd95b7d577d64bb1688dec29045917
Gary Venter <office@all-sorts.biz>
2025-02-21 08:17:04 +1300

    txp-listtables

--- five/assets/js/five.js
+++ five/assets/js/five.js
@@ -4,27 +4,37 @@ var $;
 $(document).ready(function() {
   // temp, demo only
   $('.txp-save').click(function() {
-    $('.messageflash').css('display','block');
+    $('.messageflash').css('display', 'block');
   });
 
   // temp hack to override jqui behaviour
   $('.toggle[style]').css('display', 'flex');
 
-//   function debounce(func, timeout = 100) {
-//     let timer;
-//     return (...args) => {
-//       clearTimeout(timer);
-//       timer = setTimeout(() => { func.apply(this, args); }, timeout);
-//     };
-//   }
-//
-//   function init() {
-//
-//   }
-//
-//   const processChange = debounce(() => init());
-//   window.addEventListener("resize", processChange);
-//
-//   init();
+
+  function debounce(func, timeout = 100) {
+    let timer;
+    return (...args) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => { func.apply(this, args); }, timeout);
+    };
+  }
+
+  function init() {
+    // add a class to .txp-listtables if it overflows
+    let listtables = $('.txp-listtables');
+    listtables.find('table').each(function() {
+      let tablew = $(this).find('tr:first').width();
+      if (tablew > $(this).width()) {
+        listtables.addClass('is--overflowing');
+      } else {
+        listtables.removeClass('is--overflowing');
+      }
+    });
+  }
+
+  const processChange = debounce(() => init());
+  window.addEventListener("resize", processChange);
+
+  init();
 
 });
